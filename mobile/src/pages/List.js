0cd2d5ec1b54993c7eec9989caf5c10811da552d
Felipe Livino dos santos <felipelivino@Mac-mini-de-Felipe.local>
2019-10-12 23:07:00 -0300

    first commit

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import socketio from 'socket.io-client';
+import { Alert,SafeAreaView, ScrollView, Text, Image, AsyncStorage, StyleSheet } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png'
+
+export default function Login(){
+
+    const [techs,setTechs] =  useState([]);
+    
+    useEffect(()=>{
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.0.107:3333',{
+                query: {user_id}
+            })
+
+            socket.on('booking_response', booking =>{
+                Alert.alert(`Sua reserva em ${booking.spot.comany} em ${booking.date} foi ${booking.approved ? 'APROVADA': 'REJEITADA'}`);
+            })
+        })
+
+        
+    },[]);
+
+    useEffect(()=>{
+        AsyncStorage.getItem('techs').then(storageTechs =>{
+            const techArray = storageTechs.split(',').map(tech => tech.trim());
+            setTechs(techArray);
+
+        });
+    });
+
+    return <SafeAreaView style={StyleSheet.container}>
+        <Image  style={styles.logo} source={logo}/>
+        <ScrollView>
+            {techs.map(tech => <SpotList key={tech} tech={tech}/>)}
+        </ScrollView>
+        
+    </SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+    },
+    logo:{
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 30,
+    }
+});
